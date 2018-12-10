c3ac9f651ef225ec74c9245bc86b9fc2a0927ad9
yasin <123@qq.com>
2018-12-10 18:34:29 +0800

    fix

--- main.go
+++ main.go
@@ -11,15 +11,25 @@ import (
 	"flag"
 	"net/http"
 	"net"
+	"strings"
 )
+
+type ContainerStatus struct {
+	ID      string `json:"Id"`
+	Name    string
+	Created string
+	State   string
+	Status  string
+}
 
 type SocketClient struct {
 	Online     bool
 	RemoteAddr net.Addr
-	clientMes  chan []types.Container
+	ClusterID  string
+	clientMes  chan []ContainerStatus
 }
 
-var addr = flag.String("addr", "localhost:8000", "http service address")
+var addr = flag.String("addr", "localhost:8001", "http service address")
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
@@ -30,17 +40,29 @@ var clientConns = make(map[*websocket.Conn]*SocketClient)
 func main() {
 	flag.Parse()
 	ctx := context.Background()
-	cli, _ := client.NewClientWithOpts(client.WithHost("tcp://master3g7.cs.cn-shenzhen.aliyuncs.com:20119"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/test/ca.pem", "/Users/moclaf/.acs/certs/test/cert.pem", "/Users/moclaf/.acs/certs/test/key.pem"))
+	apiCli, _ := client.NewClientWithOpts(client.WithHost("tcp://master1g7.cs.cn-shenzhen.aliyuncs.com:20033"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/api/ca.pem", "/Users/moclaf/.acs/certs/api/cert.pem", "/Users/moclaf/.acs/certs/api/key.pem"))
+	asyncCli, _ := client.NewClientWithOpts(client.WithHost("tcp://master1g7.cs.cn-shenzhen.aliyuncs.com:20004"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/async/ca.pem", "/Users/moclaf/.acs/certs/async/cert.pem", "/Users/moclaf/.acs/certs/async/key.pem"))
+	indiaCli, _ := client.NewClientWithOpts(client.WithHost("tcp://master1g7.cs.cn-shenzhen.aliyuncs.com:20025"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/prod/ca.pem", "/Users/moclaf/.acs/certs/prod/cert.pem", "/Users/moclaf/.acs/certs/prodcat/key.pem"))
 
-	time_interval, _ := strconv.Atoi("5")
-	ticker := time.NewTicker(time.Second * time.Duration(time_interval))
+	clis := map[string]*client.Client{
+		"api": apiCli,
+		"async": asyncCli,
+		"india": indiaCli,
+	}
+
+
+	time_interval, _ := strconv.Atoi("1")
+	ticker := time.NewTicker(time.Minute * time.Duration(time_interval))
 
 	go func() {
 		for _ = range ticker.C {
-			message := getcontainerlist(cli, ctx)
-			for k,v := range clientConns {
-				log.Println("now is client", clientConns[k].RemoteAddr,clientConns[k].Online)
+			for k, v := range clientConns {
+				log.Println("now is client", clientConns[k].RemoteAddr, clientConns[k].Online)
 				if v.Online {
+					var message []ContainerStatus
+					for _, item := range getcontainerlist(clis[v.ClusterID], ctx) {
+						message = append(message, ContainerStatus{item.ID, strings.Split(item.Names[0], "/")[len(strings.Split(item.Names[0], "/"))-1], time.Unix(item.Created, 0).Format("2006-01-02 15:04:05"), item.State, item.Status})
+					}
 					v.clientMes <- message
 				} else if !v.Online {
 					delete(clientConns, k)
@@ -50,6 +72,7 @@ func main() {
 	}()
 	log.Println("serverstart")
 	http.HandleFunc("/echo", echo)
+	//http.HandleFunc("/dockerlog", dockerlog)
 	//http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
@@ -67,6 +90,10 @@ func getcontainerlist(client *client.Client, ctx context.Context) []types.Contai
 //	http.ServeFile(w, r, "./index.html")
 //}
 
+func dockerlog() {
+
+}
+
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -74,13 +101,13 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer c.Close()
-	sc := &SocketClient{Online: true, RemoteAddr: c.RemoteAddr(), clientMes: make(chan []types.Container, 5)}
-	clientConns[c]=sc
-	log.Println(clientConns[c].RemoteAddr,"has connected")
+	sc := &SocketClient{Online: true, RemoteAddr: c.RemoteAddr(), clientMes: make(chan []ContainerStatus, 5)}
+	clientConns[c] = sc
+	log.Println(clientConns[c].RemoteAddr, "has connected")
 	go getClientOnlineStatus(*c, sc)
 	for {
-		mes := <- sc.clientMes
-		err = c.WriteJSON(mes[0].Status)
+		mes := <-sc.clientMes
+		err = c.WriteJSON(mes)
 		if err != nil {
 			log.Println("write:", err)
 			break
@@ -90,13 +117,15 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
 func getClientOnlineStatus(c websocket.Conn, sc *SocketClient) {
 	for {
-		_, _,err := c.ReadMessage()
+		_, req, err := c.ReadMessage()
 		if err != nil {
 			sc.Online = false
 			log.Println("read:", err)
-			log.Println(sc.RemoteAddr,"has disconnected")
+			log.Println(sc.RemoteAddr, "has disconnected")
 			c.Close()
 			break
+		}else {
+			sc.ClusterID = string(req)
 		}
 	}
 }
