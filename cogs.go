91420af65bdf57017d2d09f29b985ece390e1bf2
yasin <123@qq.com>
2018-12-17 18:52:13 +0800

    add logs

--- cogs.go
+++ cogs.go
@@ -4,23 +4,42 @@ import (
 	"github.com/docker/docker/client"
 	"context"
 	"github.com/docker/docker/api/types"
-					"io"
-			"bytes"
 	"fmt"
-	"strconv"
 	"time"
+	"strconv"
+	"io"
+	"os"
 )
 
 func main() {
+	m, _ := time.ParseDuration("-1h")
+	now := time.Now()
+	m1 := now.Add(m)
+
+	fmt.Println(m1)
 	ctx := context.Background()
 	cli, _ := client.NewClientWithOpts(client.WithHost("tcp://master3g7.cs.cn-shenzhen.aliyuncs.com:20119"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/test/ca.pem", "/Users/moclaf/.acs/certs/test/cert.pem", "/Users/moclaf/.acs/certs/test/key.pem"))
-	reader, _ := cli.ContainerLogs(ctx, "985beeb97d65", types.ContainerLogsOptions{
+	reader, _ := cli.ContainerLogs(ctx, "2dc73402269a", types.ContainerLogsOptions{
 		ShowStdout: true,
 		ShowStderr: true,
-		Since: strconv.Itoa(int(time.Now().Unix())),
+		Since:      strconv.FormatInt(m1.Unix(), 10),
+		Follow:     true,
 	})
 
-	var b bytes.Buffer
-	io.Copy(&b, reader)
-	fmt.Println(b.String())
+	p := make([]byte, )
+
+	defer cli.Close()
+	for {
+		n, err := reader.Read(p)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Print(string(p[:n])) //should handle any remainding bytes.
+				break
+			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		//fmt.Printf(string(p[:n]))
+		fmt.Println(n)
+	}
 }
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"net"
 	"strings"
+	"io"
 )
 
 type ContainerStatus struct {
@@ -22,11 +23,18 @@ type ContainerStatus struct {
 	Status  string
 }
 
+type ctnLogOpt struct {
+	containerID string
+	logSwitch   bool
+}
+
 type SocketClient struct {
 	Online     bool
 	RemoteAddr net.Addr
 	ClusterID  string
 	clientMes  chan []ContainerStatus
+	logStream  chan []byte
+	ctnLogOpt  *ctnLogOpt
 }
 
 var addr = flag.String("addr", "localhost:8001", "http service address")
@@ -39,7 +47,6 @@ var clientConns = make(map[*websocket.Conn]*SocketClient)
 
 func main() {
 	flag.Parse()
-	ctx := context.Background()
 	apiCli, _ := client.NewClientWithOpts(client.WithHost("tcp://master1g7.cs.cn-shenzhen.aliyuncs.com:20033"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/api/ca.pem", "/Users/moclaf/.acs/certs/api/cert.pem", "/Users/moclaf/.acs/certs/api/key.pem"))
 	asyncCli, _ := client.NewClientWithOpts(client.WithHost("tcp://master1g7.cs.cn-shenzhen.aliyuncs.com:20004"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/async/ca.pem", "/Users/moclaf/.acs/certs/async/cert.pem", "/Users/moclaf/.acs/certs/async/key.pem"))
 	indiaCli, _ := client.NewClientWithOpts(client.WithHost("tcp://master1g7.cs.cn-shenzhen.aliyuncs.com:20025"), client.WithTLSClientConfig("/Users/moclaf/.acs/certs/prod/ca.pem", "/Users/moclaf/.acs/certs/prod/cert.pem", "/Users/moclaf/.acs/certs/prod/key.pem"))
@@ -50,30 +57,50 @@ func main() {
 		"india": indiaCli,
 	}
 
-	time_interval, _ := strconv.Atoi("5")
+	var ctx = context.Background()
+	logDelay, _ := time.ParseDuration("-10m")
+	time_interval, _ := strconv.Atoi("10")
 	ticker := time.NewTicker(time.Second * time.Duration(time_interval))
 
+	// push msg of cluster status to all client
 	go func() {
 		for _ = range ticker.C {
 			for k, v := range clientConns {
-				log.Println("now is client", clientConns[k].RemoteAddr, clientConns[k].Online)
-				if v.Online {
-					var message []ContainerStatus
-					for _, item := range getcontainerlist(clis[v.ClusterID], ctx) {
-						message = append(message, ContainerStatus{item.ID, strings.Split(item.Names[0], "/")[len(strings.Split(item.Names[0], "/"))-1], time.Unix(item.Created, 0).Format("2006-01-02 15:04:05"), item.State, item.Status})
+				go func() {
+					if !v.pushStatusMes(clis, ctx) {
+						delete(clientConns, k)
 					}
-					v.clientMes <- message
-				} else if !v.Online {
-					delete(clientConns, k)
-				}
+					log.Println(v)
+					judge, err := v.pushCtnLogs(clis, ctx, logDelay)
+					if judge {
+						if err != nil {
+							v.ctnLogOpt = nil
+						}
+					}
+				}()
 			}
 		}
 	}()
+
+	//push msg of containers logs to client
+
 	log.Println("serverstart")
 	http.HandleFunc("/echo", echo)
 	//http.HandleFunc("/dockerlog", dockerlog)
 	//http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func (sc *SocketClient) pushStatusMes(clis map[string]*client.Client, ctx context.Context) bool {
+	log.Println("now is client", sc.RemoteAddr, sc.Online)
+	if sc.Online {
+		var message []ContainerStatus
+		for _, item := range getcontainerlist(clis[sc.ClusterID], ctx) {
+			message = append(message, ContainerStatus{item.ID, strings.Split(item.Names[0], "/")[len(strings.Split(item.Names[0], "/"))-1], time.Unix(item.Created, 0).Format("2006-01-02 15:04:05"), item.State, item.Status})
+		}
+		sc.clientMes <- message
+	}
+	return sc.Online
 }
 
 func getcontainerlist(client *client.Client, ctx context.Context) []types.Container {
@@ -84,12 +111,32 @@ func getcontainerlist(client *client.Client, ctx context.Context) []types.Contai
 	return res
 }
 
-//func home(w http.ResponseWriter, r *http.Request) {
-//	http.ServeFile(w, r, "./index.html")
-//}
-
-func getdockerlogs(cli *client.Client, containerID string) {
-
+func (sc *SocketClient) pushCtnLogs(clis map[string]*client.Client, ctx context.Context, logDelay time.Duration) (bool, error) {
+	if sc.ctnLogOpt == nil {
+		return false, nil
+	}
+	reader, err := clis[sc.ClusterID].ContainerLogs(ctx, sc.ctnLogOpt.containerID, types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Since:      strconv.FormatInt(time.Now().Add(logDelay).Unix(), 10),
+		Follow:     true,
+	})
+	if err != nil {
+		return true, err
+	}
+	defer reader.Close()
+	m := make([]byte, 256)
+	for {
+		n, err := reader.Read(m)
+		if err != nil {
+			if err == io.EOF {
+				sc.logStream <- m[:n]
+			}
+			return true, err
+		}
+		log.Println(m[:n])
+		sc.logStream <- m[:n]
+	}
 }
 
 func echo(w http.ResponseWriter, r *http.Request) {
@@ -99,21 +146,32 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer c.Close()
-	sc := &SocketClient{Online: true, RemoteAddr: c.RemoteAddr(), clientMes: make(chan []ContainerStatus, 5), ClusterID: "api"}
+	sc := &SocketClient{Online: true, RemoteAddr: c.RemoteAddr(), clientMes: make(chan []ContainerStatus, 5), ClusterID: "api", logStream: make(chan []byte), ctnLogOpt:nil }
 	clientConns[c] = sc
 	log.Println(clientConns[c].RemoteAddr, "has connected")
-	go getClientOnlineStatus(*c, sc)
+	go sc.getClientOnlineStatus(*c)
+	go func() {
+		for {
+			ctnLog := <-sc.logStream
+			log.Println(string(ctnLog))
+			err = c.WriteJSON(string(ctnLog))
+			if err != nil {
+				log.Println("write log error:", err)
+				sc.ctnLogOpt = nil
+			}
+		}
+	}()
 	for {
 		mes := <-sc.clientMes
 		err = c.WriteJSON(mes)
 		if err != nil {
-			log.Println("write:", err)
+			log.Println("write status error:", err)
 			break
 		}
 	}
 }
 
-func getClientOnlineStatus(c websocket.Conn, sc *SocketClient) {
+func (sc *SocketClient) getClientOnlineStatus(c websocket.Conn) {
 	for {
 		_, req, err := c.ReadMessage()
 		if err != nil {
@@ -123,7 +181,11 @@ func getClientOnlineStatus(c websocket.Conn, sc *SocketClient) {
 			c.Close()
 			break
 		} else {
-			if len(req) == 64 {
+			if len(req) >= 64 {
+				sc.ctnLogOpt = &ctnLogOpt{
+					containerID: string(req),
+					logSwitch:   true,
+				}
 			} else {
 				sc.ClusterID = string(req)
 			}
